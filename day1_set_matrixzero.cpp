3c0005d7283d832450108193ef2216a3a4b92670
Shailja <genieshailja@gmail.com>
2023-06-08 18:47:25 +0530

    Update day1_set_matrixzero.cpp

--- day1_set_matrixzero.cpp
+++ day1_set_matrixzero.cpp
@@ -1,3 +1,38 @@
+//codestudio
+#include <bits/stdc++.h>
+
+void setZeros(vector<vector<int>> &matrix)
+{
+ int rows=matrix.size();
+ int col=matrix[0].size();
+  int col0=1;
+  for(int i=0;i<rows;i++){
+    if(matrix[i][0]==0)col0=0;
+    for(int j=1;j<col;j++){
+      if(matrix[i][j]==0)
+      {
+        matrix[i][0]=0;
+        matrix[0][j]=0;
+      }
+    }
+  }
+  for(int i=rows-1;i>=0;i--){
+    for(int j=col-1;j>0;j--){
+ 
+     if(matrix[i][0]==0||matrix[0][j]==0)
+        matrix[i][j]=0;
+    
+    }
+  }if(col0==0){
+     for(int i=rows-1;i>=0;i--){
+    matrix[i][0]=0;
+  }
+  }
+ 
+}
+
+
+//leetcode
 //time complexity =O(2*x*y)
 //space complexity =O(x)+O(y)
 class Solution {
@@ -24,3 +59,6 @@ public:
         }
     }
 };
+
+
+
