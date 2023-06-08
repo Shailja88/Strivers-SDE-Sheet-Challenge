2d0627d1fb1550d5e6cdc93fe9204ab6eb411473
Shailja <genieshailja@gmail.com>
2023-06-08 18:52:20 +0530

    Update day1_besttimetobuyandsell.cpp

--- day1_besttimetobuyandsell.cpp
+++ day1_besttimetobuyandsell.cpp
@@ -1,3 +1,4 @@
+//codestudio
 #include <bits/stdc++.h> 
 int maximumProfit(vector<int> &prices){
     // Write your code here.
@@ -9,3 +10,23 @@ int maximumProfit(vector<int> &prices){
     }
     return profit;
 }
+
+//leetocode
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+     //auxillary space
+     int x=prices.size();
+     int maxi=INT_MIN;
+     int ans=INT_MIN;
+     int arr[x];
+     for(int i=x-1;i>=0;i--){
+          maxi=max(prices[i],maxi);
+         arr[i]=maxi;
+     } 
+     for(int i=0;i<x;i++){
+        ans=max(ans,(arr[i]-prices[i]));
+     }  
+     return ans;
+    }
+};
