95d1957842f4df12e6cd3d10bc4655830abedbbb
Shailja <genieshailja@gmail.com>
2023-06-08 18:48:50 +0530

    Update day1_pascaltriangle.cpp

--- day1_pascaltriangle.cpp
+++ day1_pascaltriangle.cpp
@@ -1,3 +1,4 @@
+//codestudio
 #include <bits/stdc++.h>
 
 vector<vector<long long int>> printPascal(int n) 
@@ -18,3 +19,27 @@ vector<vector<long long int>> printPascal(int n)
 
   return v;
 }
+
+//leetcode
+
+class Solution {
+public:
+vector<int>row(int n){
+    long long anss=1;
+    vector<int>ans;
+   
+    ans.push_back(1);
+    for(int i=1;i<n;i++){
+     anss=(anss*(n-i))/i;
+     ans.push_back(anss);
+    }
+    return ans;
+}
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>ans;
+        for(int i=1;i<=numRows;i++){
+            ans.push_back(row(i));
+        }
+        return ans;
+    }
+};
