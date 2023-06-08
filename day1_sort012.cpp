652221a96e52fc6d300edb1288d745bc54b0e76c
Shailja <genieshailja@gmail.com>
2023-06-08 18:51:01 +0530

    Update day1_sort012.cpp

--- day1_sort012.cpp
+++ day1_sort012.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h> 
+//codestudio
+include <bits/stdc++.h> 
 void sort012(int *arr, int n)
 {
    //   Write your code here
@@ -19,3 +20,25 @@ void sort012(int *arr, int n)
   }
 
 }
+//leetcode
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        ////dutch national flag
+        int x=nums.size();
+        int low=0,high=x-1,mid=0;  
+           while(high>=mid){
+        if(nums[mid]==0){
+          swap(nums[low],nums[mid]);
+          low++,mid++;
+        }
+         else if(nums[mid]==1){
+          mid++;
+        } 
+        else{
+          swap(nums[mid],nums[high]);
+          high--;
+        }
+           }
+    }
+};
