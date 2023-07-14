5aa86a4669fb55f1ef073fdb9e9cd0414a2d8fb2
Shailja <genieshailja@gmail.com>
2023-07-14 19:35:36 +0530

    Update day6_palindromelinkedlist.cpp

--- day6_palindromelinkedlist.cpp
+++ day6_palindromelinkedlist.cpp
@@ -37,3 +37,55 @@ bool isPalindrome(LinkedListNode<int> *head) {
    }
 return true;
 }
+
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+bool isPalindrome(LinkedListNode<int> *head) {
+    // Write your code here.
+    LinkedListNode<int> *slow=head,*fast=head;
+    if(head==NULL || head->next==NULL)return true;
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    //odd number of list ke liye
+    if(fast!=NULL){
+        slow=slow->next;
+    }
+   ///////////middle is slow
+   LinkedListNode<int> *curr=NULL;
+   while(slow && slow->next){
+       LinkedListNode<int> *temp=slow->next;
+       slow->next=curr;
+       curr=slow;
+       slow=temp;
+   }
+if(slow->next==NULL){
+    slow->next=curr;
+}
+//checkk kro ab 
+while(slow and head){
+    if(slow->data!=head->data)return false;
+    slow=slow->next;
+    head=head->next;
+}
+return true;
+}
