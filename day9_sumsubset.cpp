3f316343aada79f432349fdff8a90b3c5afedfe6
Shailja <genieshailja@gmail.com>
2023-07-17 19:32:10 +0530

    Create day9_sumsubset.cpp

---
+++ day9_sumsubset.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+   void fun(int ind,int sum,vector<int>&arr,int N,vector<int>&sumset){
+       if(ind==N){
+           sumset.push_back(sum);
+           return ;
+       }
+       fun(ind+1,sum+arr[ind],arr,N,sumset);//taken 
+       fun(ind+1,sum,arr,N,sumset);//not taken
+ 
+   }
+    
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+        // Use recursion 
+        vector<int>v;
+        fun(0,0,arr,N,v);
+        sort(v.begin(),v.end());
+         return v;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        vector<int> arr(N);
+        for(int i = 0 ; i < N ; i++){
+            cin >> arr[i];
+        }
+        Solution ob;
+        vector<int> ans = ob.subsetSums(arr,N);
+        sort(ans.begin(),ans.end());
+        for(auto sum : ans){
+            cout<< sum<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
