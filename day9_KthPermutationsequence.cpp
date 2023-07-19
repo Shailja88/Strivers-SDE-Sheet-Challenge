b7d90f63413b1c0fb8d41f7b5fbe1f2f4c81d47e
Shailja <genieshailja@gmail.com>
2023-07-19 17:13:26 +0530

    Update day9_KthPermutationsequence.cpp

--- day9_KthPermutationsequence.cpp
+++ day9_KthPermutationsequence.cpp
@@ -1,6 +1,6 @@
 /*LEETCODE*
 Time Complexity: O(n*n!) +O(n)
-Auxiliary Space: O(1)/
+Auxiliary Space: O(1)*/
 class Solution {
 public:
     string getPermutation(int n, int k) {
@@ -45,3 +45,30 @@ string kthPermutation(int n, int k) {
         }
         return s;
 }
+
+class Solution {
+public:
+/*n = 4 k =17*
+TC=O(n^2)
+SC=O(n)/                  */
+    string getPermutation(int n, int k) {
+        vector<int>v;
+        string s="";
+        int fact=1;
+        for(int i=1;i<n;i++){
+           fact=fact*i;   //1 2 6
+           v.push_back(i);  //[1 2 3 ]
+        }
+        k=k-1;// 16 "zero based indexing "
+        v.push_back(n) ;//[1 2 3 4]
+        //fact=6 
+       while(true){
+        s=s+to_string(v[k/fact]);//3 34 341 3412
+        v.erase(v.begin()+k/fact);//[1 2 4] [1 2] [2] []
+        if(v.size()==0)break;//3 2 1 0 
+         k=k%fact;//4 0 0
+         fact=fact/v.size();//2 1 1
+  }
+return s;
+    }
+};
