c5d3da75d2df5da37251b8a722ec02ba6e396858
Shailja <genieshailja@gmail.com>
2023-09-05 08:38:08 +0530

    Create day20_searchinBST.cpp

---
+++ day20_searchinBST.cpp
@@ -0,0 +1,24 @@
+/*
+    Following is the Binary Tree node structure:
+
+    template <typename T>
+    class BinaryTreeNode
+    {
+    public:
+        T data;
+        BinaryTreeNode<T> *left, *right;
+        BinaryTreeNode() : data(0), l9eft(NULL), right(NULL) {}
+        BinaryTreeNode(T x) : data(x), left(NULL), right(NULL) {}
+        BinaryTreeNode(T x, BinaryTreeNode<T> *left, BinaryTreeNode<T> *right) : data(x), left(left), right(right) {}
+    };
+
+*/
+
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+    // Write your code here.
+  while(root!=NULL && root->data!=x){
+     root= (root->data>x)?root->left:root->right;
+    
+  }
+   return root;
+}
